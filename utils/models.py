5d4f5217b638b8ff9c443b12e83835166f15a90f
Alex1Iv <ivanov.politehnik@gmail.com>
2026-07-09 13:05:36 +0600

    upd torch model

--- utils/models.py
+++ utils/models.py
@@ -1,267 +1,338 @@
-import numpy as np
+import torch.nn.functional as F
+import torch.nn as nn
+from torch.nn.utils import weight_norm
+#from torch import mean
+import torch
 import pandas as pd
-from keras.models import Model
 
-import tensorflow as tf
-# from keras.models import Sequential
-from keras.layers import LSTM, Dense
-#from utils.reader_config import config_reader
-# Import parameters
-#config = config_reader('../config/config.json')
-PATH_FIGURES = "../figures/"
-
-from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
-import os
-
-def split_sequences(X:np.array, Y:np.array, n_steps:int)->np.array:
-    """ Split a univariate sequence into samples
-
-    Args:
-        sequence (np.array): time series (1 feature)
-        n_steps (int): lag
-
-    Returns:
-        np.array: _description_
-    """    
-    X_out, y_out = list(), list()
- 
-    for i in range(len(X)):
-        # find the end of this pattern
-        end_ix = i + n_steps
-        
-        # check if we are beyond the sequence
-        if end_ix >=  len(X): #if end_ix >=  len(X)-1:
-            break
-        
-        # gather input and output parts of the pattern
-        # seq_x, seq_y = X[i:end_ix], Y[end_ix]
-        
-        X_out.append(X[i:end_ix])
-        y_out.append(Y[end_ix])
-  
-    return np.array(X_out), np.array(y_out)
-
-class ModelLSTM(Model):
-    """ Model inherits the LSTM class from Keras.
-    Параметры:
-    ----------
-    Model - data
-   
+class ResidualTCNBlock(nn.Module):
     """
-    def __init__(self, data, learning_rate):
+    Temporal Convolutional Network for one-step forecasting.
     
-        super().__init__()
-        # ------- Parameters ------------
-        _, self.n_timesteps, self.n_channels = data.shape
-        self.learning_rate = learning_rate
-        # -------- Model layers ----------------
-        self.input_channels = x = tf.keras.layers.Input(shape=(self.n_timesteps, self.n_channels)) 
-      
-        x = tf.keras.layers.LSTM(
-            units=64, 
-            return_sequences=True,     
-            dropout=0.2,
-            #recurrent_dropout=0.2
-            )(x)
-        
-        x = tf.keras.layers.LSTM(
-            units=32,    
-            dropout=0.2,
-            #recurrent_dropout=0.2
-            )(x)
-        x = tf.keras.layers.Dense(units=6)(x)
-        self.output_channels = tf.keras.layers.Dense(units=1)(x)
-        
-        print(f"input_shape = {(self.n_timesteps, self.n_channels)}\noutput_units = {(self.output_channels)}")
+    Input:
+        (batch, sequence_length, features)
+    
+    Parameters
+    ----------
+    in_channels:int
+        number of features
+    out_channels : int
+        Number of output channels.
+    kernel_size : int
+        Size of convolution kernel (features).
 
-    def build_model(self):
-        """Initiate the model
-        """
-        model = tf.keras.Model(
-            inputs=self.input_channels,
-            outputs=self.output_channels,
-            name="model_LSTM"
-        )        
-        model.summary()
+    Output:
+        (batch, out_channels, seq_len)
+    """
+
+    def __init__(        
+        self, 
+        in_channels, 
+        out_channels=32, 
+        kernel_size=3, 
+        dilation=1, 
+        dropout=0.2):
+
+        super().__init__()
+
+        self.kernel_size = kernel_size
+        self.dilation = dilation
         
-        #--------------
-        # compile model
-        #--------------
-        optimizer = tf.keras.optimizers.Adam(learning_rate = self.learning_rate)
-        
-        model.compile(
-            loss = tf.keras.losses.Huber(), #= 'mse', 
-            metrics = ['mae', 'mse'], 
-            optimizer = optimizer
+        self.conv1 = weight_norm(
+            nn.Conv1d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=self.kernel_size,
+                padding=0, #causal convolutions instead of symmentric padding
+                dilation=dilation
+            )
         )
 
-        return model
+        self.conv2 = weight_norm(
+            nn.Conv1d(
+                in_channels=out_channels,
+                out_channels=out_channels,
+                kernel_size=self.kernel_size,
+                padding=0,
+                dilation=self.dilation
+            )
+        )
+        
+        #residual (skip) connections
+        if in_channels != out_channels:
+            self.residual = nn.Conv1d(
+                in_channels,
+                out_channels,
+                kernel_size=1
+            )
+        else:
+            self.residual = nn.Identity()
+            
+        
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(dropout)
+        #self.fc = nn.Linear(out_channels, 1)
+  
+    def forward(self, x):
+        """
+        Input shape:
+            (batch, seq_len, features)
+
+        Conv1d expects:
+            (batch, features, seq_len)
+        """
+        # Save the original input
+        residual = self.residual(x)
+        
+        # X is the original input of shape (batch, n_features, seq_len) 
+        #x = x.transpose(1, 2)
+        
+        # ---------- Conv1 ----------
+        # add residual, which shape becomes (batch, out_channels, seq_len) after convolution
+        left_padding = (self.kernel_size - 1) * self.dilation #(3 - 1) * 1
+        x = F.pad(x, (left_padding, 0))
+
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        
+        # ---------- Conv2 ----------
+        #left_padding = (self.kernel_size - 1) * self.dilation*2 #(3 - 1) * 2
+        x = F.pad(x, (left_padding, 0))
+        
+        x = self.conv2(x)
+        x = x + residual # add residual
+        x = self.relu(x)
+        x = self.dropout(x)
+
+        return x
     
-
-def callbacks(
-    monitor,
-    factor,
-    mode,
-    restore_best_weights,
-    patience,
-    stop_patience,
-    verbose,
-    learning_rate,
-    model_name:str=None
-    ):
-    """Model training setup function
-
-    Args:
-        min_lr (_float_): lower boundary of the learning rate to stop training
-        monitor (str) - metric name 
-        mode (str)- modes {"auto", "min", "max"}. Max - stop training if the metric doesn't improve
-        reduce_patience (_int_): number of epochs to evaluate the learning rate improvement
-        stop_patience (_int_):  number of epochs before training will be ended
-        path_models (_str_): path to save the model
-        save_best_only (bool): If True then saves the model with the best metric.
+    
+class TCNRegressor(nn.Module):
     """
-    
-     
-    
-    # Save the best model
-    # checkpoint = ModelCheckpoint(
-    #     filepath=os.path.join(config.path_models + model_name + '.hdf5'), 
-    #     #filepath=path,
-    #     monitor=config.monitor, 
-    #     verbose=config.verbose, 
-    #     mode=config.mode, 
-    #     save_best_only=True, 
-    #     #save_weights_only=True
-    # )
-    # checkpoint = ModelCheckpoint(
-    #     filepath= '../models/model_LSTM.hdf5', #os.path.join(config.PATH_MODEL + model_name +'.hdf5'), 
-    #     monitor=config.monitor, 
-    #     verbose=1, 
-    #     mode=config.mode, 
-    #     save_best_only=True
-    # )
+    Temporal Convolutional Network for one-step forecasting.
 
+    Input shape:
+        (batch, sequence_length, n_features)
 
-    # stop training if the metric doesn't improve
-    earlystop = EarlyStopping(
-        monitor=monitor, 
-        mode=mode, 
-        patience=stop_patience, 
-        restore_best_weights=restore_best_weights
-    )
+    Output:
+        (batch, 1)
+    """
 
-    # reduce the learning rate if the metric doesn't improve
-    reduce_lr = ReduceLROnPlateau(
-        monitor=monitor, 
-        #mode=mode,  
-        factor=factor, 
-        patience=patience,  # might be 10
-        verbose=verbose, 
-        min_lr=learning_rate/1000
-    )
+    def __init__(
+        self,
+        in_channels,
+        out_channels=32,
+        kernel_size=3,
+        dropout=0.2,
+    ):
+        super().__init__()
+
+        # Stack several residual blocks with increasing dilations.
+        # Increasing the dilation enlarges the receptive field
+        # without increasing the number of parameters significantly.
+
+        self.blocks = nn.Sequential(
+
+            ResidualTCNBlock(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=kernel_size,
+                dilation=1,
+                dropout=dropout,
+            ),
+
+            # ResidualTCNBlock(
+            #     in_channels=out_channels,
+            #     out_channels=out_channels,
+            #     kernel_size=kernel_size,
+            #     dilation=2,
+            #     dropout=dropout,
+            # ),
+
+            # ResidualTCNBlock(
+            #     in_channels=out_channels,
+            #     out_channels=out_channels,
+            #     kernel_size=kernel_size,
+            #     dilation=4,
+            #     dropout=dropout,
+            # ),
+        )
+
+        # Final regression layer
+        self.regressor = nn.Linear(out_channels, 1)
+
+    def forward(self, x):
+        """
+        # x shape (batch, seq_len, features) ->
+        #         (batch, features, seq_len)
+        """
+
+        # Conv1D expects
+        # (batch, channels, sequence_length)
+        x = x.transpose(1, 2)
+
+        # Pass through all residual blocks
+        x = self.blocks(x)
+
+        # # Take the representation of the last time step
+        # x = x[:, :, -1]
+        
+        #global average pooling
+        x = torch.mean(x, dim=2) #torch.mean
+
+        # Regression output
+        x = self.regressor(x)
+
+        return x
     
-    return [#checkpoint, 
-            earlystop, reduce_lr]
-    
-    
+        
 import matplotlib.pyplot as plt
-
-
-def plot_history_regr(history:dict=None, model_name:str=None, plot_counter:int=None):
+import os
+    
+def plot_history_clf(
+    history:dict=None, 
+    PATH_FIGURES:str="../figures/", 
+    model_name:str=None, 
+    plot_counter:int=None):
     """Training history visualization
     
     Аргументы:
     history (keras.callbacks.History) - Training history data,
+    PATH_FIGURES - path to the figures directory. Default: "../figures/"
     model_name (str) - figure title. Use: model.name
     plot_counter (int) - figure id.      
     """
-    mse_train =  history.history['mse'] 
-    mse_val =  history.history['val_mse']  # validation sample
+    epochs = range(len(history['trn_loss']))
+    if len(history.keys())==4:
         
-    train_loss =  history.history['loss']
-    val_loss =  history.history['val_loss']
+        fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(11, 5))
 
-    epochs = range(len(mse_train))
+        ax[0].plot(epochs, history['trn_loss'], 'b', label='Train')
+        ax[0].plot(epochs, history['val_loss'], 'r', label='Valid')
+        ax[0].set_xlabel('Epoch', size=11)
+        ax[0].set_ylabel('Loss', size=11)
+        ax[0].set_title('Loss')
+        ax[0].legend(['train', 'val'])
 
-    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(11, 5))
-
-    ax[0].plot(epochs, train_loss, 'b', label='Train')
-    ax[0].plot(epochs, val_loss, 'r', label='Valid')
-    ax[0].set_xlabel('Epoch', size=11)
-    ax[0].set_ylabel('Loss', size=11)
-    ax[0].set_title('Loss')
-    ax[0].legend(['train', 'val'])
-
-    ax[1].plot(epochs, mse_train, 'b', label='Train')
-    ax[1].plot(epochs, mse_val, 'r', label='Valid')
-    ax[1].set_xlabel('Epoch', size=11)
-    ax[1].set_ylabel('MSE value', size=11)
-    ax[1].set_title(f"MSE")
-    ax[1].legend(['train', 'val'])
-
-    if plot_counter is not None:
-        plt.suptitle(f"Fig.{plot_counter} - {model_name} model", y=0.05, fontsize=14)
-        plt.savefig(os.path.join(PATH_FIGURES + f'fig_{plot_counter}.png'))
+        ax[1].plot(epochs, history['trn_rmse'], 'b', label='Train')
+        ax[1].plot(epochs, history['val_rmse'], 'r', label='Valid')
+        ax[1].set_xlabel('Epoch', size=11)
+        ax[1].set_ylabel('MSE value', size=11)
+        ax[1].set_title(f"MSE")
+        ax[1].legend(['train', 'val'])
     
-    else: 
-        plot_counter = 1
-        plt.suptitle(f"Fig.{plot_counter} - {model_name} model", y=-0.1, fontsize=14)  
-    plt.tight_layout();
+    else:
+        plt.figure(figsize=(6, 4))
+        
+        plt.plot(epochs, history['trn_loss'], 'b', label='Train')
+        plt.plot(epochs, history['val_loss'], 'r', label='Valid')
+        plt.xlabel('Epoch', size=11)
+        plt.ylabel('Huber Loss', size=11)
+        plt.title('Loss')
+        plt.legend(['train', 'val'])
 
+        if plot_counter is not None:
+            plt.suptitle(f"Fig.{plot_counter} - {model_name} model", y=0.05, fontsize=14)
+            plt.savefig(os.path.join(PATH_FIGURES + f'fig_{plot_counter}.png'))
+        
+        else: 
+            plot_counter = 1
+            plt.suptitle(f"Fig.{plot_counter} - {model_name} model", y=-0.1, fontsize=14)  
+        plt.tight_layout();
+        
+import numpy as np
 
-def get_predictions(
-    num_periods_to_forecast:int,
-    # period_step:int,
-    last_prediction:np.array, 
-    last_index:pd.Timestamp, 
-    scaler, 
-    model, 
-    n_steps:int, 
-    #n_features:int=1
-    )->pd.Series:
-    """Generate predictions for future periods
+def split_N_sequences(X:np.array, Y:np.array, n_steps:int)->np.array:
+    """ Split a univariate sequence into samples
 
     Args:
-        num_periods_to_forecast (int): number of periods to predict
-        period_step (int): period step
-        last_prediction (np.array): _description_
-        last_index (pd.Timestamp): _description_
-        scaler (_type_): _description_
-        model (_type_): _description_
-        n_steps (int): _description_
-        n_features (int, optional): _description_. Defaults to 1.
+        sequence (np.array): time series (N features). Shape (N, n_features)
+        n_steps (int): lag. Shape (N,)
 
     Returns:
-        pd.Series: _description_
+        np.array: _description_
+    """    
+    X_out, y_out = [], []
+ 
+    for i in range(len(X) - n_steps):
+        
+        X_out.append(X[i:i+n_steps])
+        y_out.append(Y[i+n_steps])
+  
+    return np.array(X_out), np.array(y_out)
+
+
+def get_tch_predictions(
+    num_periods_to_forecast,
+    last_prediction,
+    last_index,
+    scaler,
+    model:TCNRegressor,
+    #device,
+):
     """
-    future_indexes_array = []
-    predicted_arr = np.array([], dtype='float32')
+    Recursive forecasting using a trained PyTorch TCN.
 
-    for i in range(1, num_periods_to_forecast+1):
-        # join new indexes
-        future_indexes = last_index + pd.Timedelta(minutes=i)
-        future_indexes_array.append(future_indexes)  #*period_step
-   
-        y_hat = model.predict(last_prediction, verbose=0)#.squeeze()
-        predicted_arr = np.append(predicted_arr, y_hat)
-        
-        minutes = (future_indexes.hour*60 + future_indexes.minute)
-        sin_time = np.sin(2*np.pi * minutes/1440)
-        cos_time = np.cos(2*np.pi * minutes/1440)
-        
-        new_prediction = np.append(y_hat, [sin_time, cos_time]).reshape(1,1, n_steps)
-        
-        # append and roll the prediction
-        last_prediction = np.concatenate((last_prediction,  new_prediction), axis=1)[:,1:,:]
-    
-    predictions = pd.Series(
-        scaler.inverse_transform(predicted_arr.reshape(-1,1)).squeeze(), 
-        name='future', 
-        index=future_indexes_array)#.asfreq(freq='min')
-    
-    return predictions
+    Parameters
+    ----------
+    last_prediction : ndarray
+        Shape = (1, seq_len, n_features)
 
-def get_intervals(ts:pd.Series, ci_high:float=1.0, ci_low:float=1.0):
+    model : torch.nn.Module
+        Trained model.
+    """
+
+    model.eval()
+    future_indexes, predicted = [], []
+    window = last_prediction.float() #torch.tensor(last_prediction, dtype=torch.float32)
+
+    with torch.no_grad():
+
+        for i in range(num_periods_to_forecast):
+
+            y_hat = model(window)
+            value = y_hat.item()
+            
+            predicted.append(value)
+
+            timestamp = last_index + pd.Timedelta(minutes=i + 1)
+
+            future_indexes.append(timestamp)
+
+            minutes = timestamp.hour * 60 + timestamp.minute
+            sin_time = np.sin(2 * np.pi * minutes / 1440)
+            cos_time = np.cos(2 * np.pi * minutes / 1440)
+
+            new_step = torch.tensor(
+                [[[value, sin_time, cos_time]]],
+                dtype=torch.float32,
+                #device=device
+            )
+
+            window = torch.cat((window[:, 1:, :], new_step, ),dim=1)
+
+    prediction = scaler.inverse_transform(
+        np.array(predicted).reshape(-1, 1)
+    ).squeeze()
+
+    return pd.Series(
+        prediction,
+        index=future_indexes,
+        name="future",
+    )
+    
+def get_intervals(ts:pd.Series, ci_low:float=1.0, ci_high:float=1.0):
+    """Create confidence interval
+
+    Args:
+        ts (pd.Series): time series
+        ci_high (float, optional): upper boundary. Defaults to 1.0.
+        ci_low (float, optional): lower boundary. Defaults to 1.0.
+
+    Returns:
+        _type_: Parameter with confidence interval
+    """    
     df = pd.DataFrame(ts)
     df['ci_low' ] = ts - ci_low  * ts.std()
     df['ci_high'] = ts + ci_high * ts.std()
